58c178606a26184eec3294208d2a8c451c427011
Raseena Maria Saji <raseenamariasaji@gmail.com>
2025-01-21 19:49:42 +0530

    income_tax

--- income_tax.py
+++ income_tax.py
@@ -1,12 +1,16 @@
 #Income Tax Calculation
 income = float(input("Enter your (annual)salary:"))
 tax =0
-if income<= 250000:
-    tax=0
-elif income<=500000:
-    tax=((income-250000)*0.05)
-elif income<=1000000:
-    tax=(((income-500000)*0.2)+(250000*0.05))
+if income<=300000:
+    print("No tax")
+elif income>300000 and income<=700000:
+    tax=(income-300000)*0.05
+elif income>700000 and income<=1000000:
+    tax=20000+(income-700000)*0.10
+elif income>1000000 and income<=1200000:
+    tax=50000+(income-1000000)*0.15
+elif income>1200000 and income<=1500000:
+    tax=80000+(income-1200000)*0.20
 else:
-    tax=(((income-1000000)*0.3)+(500000*0.2)+(250000*0.05))
-print("Total payable income tax:",tax)
+    tax=140000+(income-1500000)*0.30
+print("Tax paid: ",tax)
