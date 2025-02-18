89f3099b0d9245e575141e931468e6e6c2b313ce
Raseena Maria Saji <raseenamariasaji@gmail.com>
2025-02-18 07:15:44 +0530

    star

--- star_turtle.py
+++ star_turtle.py
@@ -2,13 +2,15 @@ from turtle import Turtle
 
 def star(t,length):
     t.down()
+    t.fillcolor("yellow")
+    t.begin_fill()
     t.right(75)
     t.forward(length)
     for count in range(4):
-        t.down()
         t.left(144)
         t.forward(length)
-        
+    t.end_fill()
+
 def pattern(t,length,star,num):  
     angle=360/num
     for count in range(num):
@@ -17,7 +19,6 @@ def pattern(t,length,star,num):
         t.right(cur_angle)
         t.forward(length*2)  
         t.left(cur_angle)
-        t.down()
         star(t,length)
         t.up()
         t.home()
@@ -27,5 +28,3 @@ num = int(input("Enter the no of stars:"))
 length = int(input("Enter the length:"))
 t = Turtle()
 pattern(t,length,star,num)
-    
-    
