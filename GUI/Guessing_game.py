186567d198832769589c5d20443c5c7e9cf17d65
Raseena Maria Saji <raseenamariasaji@gmail.com>
2025-03-17 23:15:14 +0530

    guess

--- GUI/Guessing_game.py
+++ GUI/Guessing_game.py
@@ -4,16 +4,16 @@ from breezypythongui import EasyFrame
 class GuessingGame(EasyFrame):
     
     def __init__(self):
-        EasyFrame.__init__(self, title="Guessing Game",height=500,width=700)
+        EasyFrame.__init__(self, title="Guessing Game",height=250,width=300)
         self.myNumber = random.randint(1, 100)
         self.count = 0
 
         greeting = "Guess a number between 1 and 100."
         self.hintLabel = self.addLabel(text=greeting, row=0, column=0, sticky="NSEW", columnspan=2)
-        self.addLabel(text="Your guess", row=1, column=0)
-        self.guessField = self.addIntegerField(0, row=1, column=1)
-        self.nextButton = self.addButton(text="Next", row=2, column=0, command=self.nextGuess)
-        self.newButton = self.addButton(text="New game", row=2, column=1, command=self.newGame)
+        self.addLabel(text="Your guess", row=2, column=0)
+        self.guessField = self.addIntegerField(0, row=2, column=1)
+        self.nextButton = self.addButton(text="Next", row=3, column=0, command=self.nextGuess)
+        self.newButton = self.addButton(text="New game", row=3, column=1, command=self.newGame)
 
     def nextGuess(self):
         self.count += 1
